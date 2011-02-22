9b75ff1720e0595f354c3d3b75e6a4e051f54287
DL88250@gmail.com <DL88250@gmail.com@9fe9a86e-49ff-ea25-d89f-df74cf3e6790>
2011-02-22 08:39:53 +0000

    IM init.

---
+++ trunk/b3log-symphony-im/src/test/java/org/b3log/symphony/im/qq/QQTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2009, 2010, 2011, B3log Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.b3log.symphony.im.qq;
+
+import org.b3log.symphony.im.util.Symphonys;
+import org.b3log.symphony.im.Keys;
+import org.b3log.symphony.im.Message;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+/**
+ * {@link QQ} test case.
+ *
+ * @author <a href="mailto:DL88250@gmail.com">Liang Ding</a>
+ * @version 1.0.0.1, Feb 22, 2011
+ */
+public final class QQTestCase {
+
+    /**
+     * QQ.
+     */
+    private QQ qq = new QQ(Symphonys.get("qqRobot1Account"),
+                           Symphonys.get("qqRobot1Pwd"));
+
+    /**
+     * Tests methods {@linkplain QQ#login()} and
+     * {@linkplain QQ#send(org.json.JSONObject)}.
+     * 
+     * @throws Exception exception
+     */
+    @Test
+    public void login() throws Exception {
+        final JSONObject loginResult = qq.login();
+        assertTrue(loginResult.getBoolean(Keys.STATUS_CODE));
+
+        final JSONObject message = new JSONObject();
+        message.put(Message.MESSAGE_TO_ACCOUNT, "845765");
+        message.put(Message.MESSAGE_CONTENT, "Test!");
+
+        final JSONObject sendResult = qq.send(message);
+
+    }
+}
